3b1d518b44fce1f05fdea0dc5e54ebcd333ad72f
Cathy Dao <cathydao95@gmail.com>
2023-08-28 23:53:41 -0700

    create get routes

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-console.log("server is running");
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/api/weather", (req, res) => {
+  console.log(req.query);
+  const city = req.query.city;
+  const apiKey = process.env.API_KEY;
+  // const params = new URLSearchParams({
+  //   q: req.query.cityName,
+  //   appid: process.env.API_KEY,
+  //   units: "imperial",
+  // });
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({ data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("HELLO CATHY");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
