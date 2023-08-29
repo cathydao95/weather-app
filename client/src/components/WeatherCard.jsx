72f47e53a593af06ed12ab84c620a898367e78e4
Cathy Dao <cathydao95@gmail.com>
2023-08-28 23:54:42 -0700

    create weather card and pass props

---
+++ client/src/components/WeatherCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./WeatherCard.css";
+
+const WeatherCard = ({ data, city }) => {
+  if (data && city) {
+    return (
+      <div className="cityWeatherInfo">
+        <h2 className="cityName info">
+          <span>City:</span> {data.name}
+        </h2>
+        <p className="weather info">
+          <span className="infoHeading">Description:</span>{" "}
+          {data.weather[0].description}
+        </p>
+        <p className="temperature info">
+          <span className="infoHeading">Temperature:</span> {data.main.temp}
+        </p>
+        <p className="feelsLike info">
+          <span className="infoHeading">Feels Like: </span>
+          {data.main.feels_like}
+        </p>
+      </div>
+    );
+  }
+};
+
+export default WeatherCard;
