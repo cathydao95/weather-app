a23238521ff33fb99a44b3e047a9d768388837ca
Cathy Dao <cathydao95@gmail.com>
2023-08-31 13:22:00 -0700

    if no city found, display text

--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -2,33 +2,35 @@ import React from "react";
 import "./WeatherCard.css";
 
 const WeatherCard = ({ data, city }) => {
-  if (data && city) {
-    return (
-      <div className="cityWeatherInfo">
-        <h2 className="cityName info">
-          <span>City:</span> {data.name}
-        </h2>
-        <p className="weather info">
-          <span className="infoHeading">Description:</span>{" "}
-          {data.weather[0].description}
-        </p>
-        <img
-          src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-        />
-        <p className="temperature info">
-          <span className="infoHeading">Temperature:</span> {data.main.temp}
-        </p>
-        <p className="feelsLike info">
-          <span className="infoHeading">Feels Like: </span>
-          {data.main.feels_like}
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="noCity">No city found. Please enter a valid city</div>
-    );
-  }
+  console.log("data", data);
+  return (
+    <div>
+      {data && data.name && (
+        <div className="cityWeatherInfo">
+          <h2 className="cityName info">
+            <span>City:</span> {data.name}
+          </h2>
+          <p className="weather info">
+            <span className="infoHeading">Description:</span>{" "}
+            {data.weather[0].description}
+          </p>
+          <img
+            src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+          />
+          <p className="temperature info">
+            <span className="infoHeading">Temperature:</span> {data.main.temp}
+          </p>
+          <p className="feelsLike info">
+            <span className="infoHeading">Feels Like: </span>
+            {data.main.feels_like}
+          </p>
+        </div>
+      )}
+      {data && data.message && (
+        <p className="noCity">No such city. Please enter a valid city</p>
+      )}
+    </div>
+  );
 };
 
 export default WeatherCard;
